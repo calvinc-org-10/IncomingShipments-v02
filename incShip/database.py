b1af7ccf8e57e62535b2f94959721eb998d55203
Calvin C <curtindolph.calvin.10@gmail.com>
2025-03-09 21:22:03 -0500

    Moving db operations to QtSql

---
+++ incShip/database.py
@@ -0,0 +1,20 @@
+from PySide6.QtSql import (QSqlDatabase, QSqlQuery )
+
+cMenu_dbName = 'W:\\Logistics\\db\\hbl.sqlite'
+
+
+class incShipdb(QSqlDatabase):
+    def __init__(self):
+        dbDriver = 'QSQLITE'
+        connectName = 'con_incShip'
+        super().__init__()
+        # con = PQconnectdb("host=server user=bart password=simpson dbname=springfield")
+        # drv = QPSQLDriver(con)
+        self.addDatabase(dbDriver, connectName)
+        self.setDatabaseName(cMenu_dbName)
+        self.open()      # this should be checked for success, but for now, take the errors raised if bad
+        # query = QSqlQuery()
+        # query.exec("SELECT NAME, ID FROM STAFF")        
+        # super().__init__(dbDriver)
+
+incShipDatabase = incShipdb()
